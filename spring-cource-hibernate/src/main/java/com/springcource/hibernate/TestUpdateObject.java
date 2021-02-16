ce7430eaa2379fcab510ba2093c9aa5d09b89387
L3v1strauss <L3v1s@mail.ru>
2021-02-16 16:50:32 +0300

    Spring Hibernate + Update/Delete object

--- spring-cource-hibernate/src/main/java/com/springcource/hibernate/TestUpdateObject.java
+++ spring-cource-hibernate/src/main/java/com/springcource/hibernate/TestUpdateObject.java
@@ -5,28 +5,24 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class TestEmployee2 {
+public class TestUpdateObject {
     public static void main(String[] args) {
         SessionFactory sessionFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
                 .buildSessionFactory();
         try {
-            Employee employee = new Employee("Dzmitry", "Nagiev", "Actor", 9000);
             Session currentSession = sessionFactory.getCurrentSession();
             currentSession.beginTransaction();
-            currentSession.save(employee);
-//            currentSession.getTransaction().commit();
-
-            int myId = employee.getId();
-//            currentSession = sessionFactory.getCurrentSession();
-//            currentSession.beginTransaction();
-            Employee employeeGenerated = currentSession.get(Employee.class, myId);
+            Employee employee1 = currentSession.get(Employee.class, 1);
+            employee1.setSalary(1000000);
+            currentSession.createQuery("update Employee set salary = 8000 where name = 'Lolita'").executeUpdate();
+//            Employee employee2 = currentSession.get(Employee.class, 4);
+//            currentSession.delete(employee2);
+            currentSession.createQuery("delete Employee where name = 'Stephany'" ).executeUpdate();
             currentSession.getTransaction().commit();
-            System.out.println(employeeGenerated);
-
         } finally {
             sessionFactory.close();
         }
+        }
     }
-}
